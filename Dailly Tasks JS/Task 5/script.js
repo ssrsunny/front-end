5da1f4efa958b9a5cb32798cf6dd024b82044b5b
ssrsunny <sunnyssr95@gmail.com>
2024-05-21 17:01:10 +0530

    modified task 5

--- Dailly Tasks JS/Task 5/script.js
+++ Dailly Tasks JS/Task 5/script.js
@@ -1,17 +1,19 @@
 function season(id) {
+  document.getElementById("front").style.display = "none";
+  document.getElementById("back").style.display = "block";
   myid = document.getElementById("button1");
   if (id == button1) {
     document.getElementById("imag").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
   } else if (id == button2) {
     document.getElementById("imag").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
   } else if (id == button3) {
     document.getElementById("imag").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
   } else if (id == button4) {
     document.getElementById("imag").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
   } else {
     null;
   }
